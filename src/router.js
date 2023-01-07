16df0a67f1ca6fb6e02fdd3ac4c19ef798220d5e
Kyz901 <kyz9.01.01@gmail.com>
2023-01-07 21:08:48 +0200

    changes url to v1/

--- src/router.js
+++ src/router.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 const userRouter = require('./users/user.router');
 
-router.use('/users', userRouter);
+router.use('/v1/users', userRouter);
 
 module.exports = router;
